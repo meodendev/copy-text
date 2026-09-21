b1f4ab0aa81c774a6c8e203ec853321d535d20fa
meodendev <meoden.dev@gmail.com>
2026-09-21 18:52:54 +0700

    Update DB_URL for Firebase Realtime Database

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   APP_NAME: "ClipSync",
   // Firebase Realtime Database URL, e.g. "https://YOUR-PROJECT-default-rtdb.firebaseio.com" (see README)
-  DB_URL: "",
+  DB_URL: "https://clipsync-a86e8-default-rtdb.asia-southeast1.firebasedatabase.app/",
   MAX_TEXT_LENGTH: 10000,
   MAX_CODE_LENGTH: 64
 };
